d3e83da3190391b7c380a6875dcf5c01614dd0ef
sameer2831 <sameerb2831@gmail.com>
2025-04-11 00:42:52 -0400

    changed BrowserRouter to hashbrowser

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter  as Router, Routes, Route } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import Preloader from "./components/Pre";
 import Footer from "./components/Footer";
